61b0c075a05c48ceebb9676bb5e6514dcb96bb92
Sl00x <30964327+Sl00x@users.noreply.github.com>
2024-02-24 22:26:24 +0100

    Delete corsiboat-api directory

--- corsiboat-api/utils/pass.crypt.ts
+++
@@ -1,9 +0,0 @@
-import * as bcrypt from "bcrypt";
-import { CreateUserDto } from "src/user/dto/create-user.dto";
-import { UpdateUserDto } from "src/user/dto/update-user.dto";
-import { MD5 } from "crypto-js";
-
-export const passcrypt = async (data: CreateUserDto) => {
-  const encryptedPassword = MD5(data.password).toString();
-  return encryptedPassword;
-};
